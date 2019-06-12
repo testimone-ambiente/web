d5645044d42d4c7372779310bca8ff6f50203999
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2019-06-12 12:43:39 +0000

    Dep.txt

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,151 +1,278 @@
 /* -- Initial configuration -- */
 
 var map = L.map('map', {zoomControl: false}).setView([42.846828, 10.579184], 5.5);
-var layer = L.esri.basemapLayer('Topographic').addTo(map);
-new L.Control.Zoom({position: 'topright'}).addTo(map)
 
+var layer = L.esri.basemapLayer('ImageryFirefly').addTo(map);			//Initializes satellitar map + labels
+layerLabels = L.esri.basemapLayer('ImageryLabels');
+map.addLayer(layerLabels);
 
-/* -- Socket for realtime updates -- */
+new L.Control.Zoom({position: 'topright'}).addTo(map);						//add zoom controls
+
+/* Global Variables */
+
+var markerStartup = [];
+var marker;
+var markerDefaultGroup;
+
+var titleMarker = [];
+var addressMarker = [];
+
+var region_markers = {
+	"Abruzzo": [], "Piemonte": [], "Sardegna": [], "Liguria": [], "Emilia-Romagna": [], "Valle d'Aosta": [],
+	"Trentino": [],  "Lombardia": [], "Campania": [], "Lazio": [], "Veneto": [],  "Sicilia": [], "Calabria": [],
+	"Toscana": [], "Marche": [],  "Umbria": [], "Friuli": [], "Molise": [],  "Puglia": [], "Basilicata": []
+}
+
+/* -- Marker display class -- */
 
 var socket = io();
-var markerStartup = [];
 
-console.log(socket);
+class Display {
 
-socket.on('report', report => {
+    dynamicDisplay() {
 
-	console.log(report);
-	var date = new Date(report.report_date * 1000);
-	console.log(report.report_date);
-	var day = date.getDay();
-	var month = date.getMonth();
-	marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
-	marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}<br><a href='/report/${report.report_id}'>Visualizza segnalazione</a>`);
-	markerStartup.push(marker);
-	myMarkers = L.layerGroup(markerStartup).addTo(map);
+        socket.on('report', report => {
 
-})
+            marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
+            marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}<br>`);
+            markerStartup.push(marker);
+            markerDefaultGroup = L.layerGroup(markerStartup).addTo(map);
 
-/* -- GET request for static display -- */
+        })
 
-function staticDisplay(){
+    }
 
- $.get("/api/data/getReports", reports => {
+    staticDisplay() {
 
-	 console.log(reports);
+        $.get("/api/data/getReports", reports => {
 
-	reports.map(report => {
+            reports.map(report => {
 
-		console.log(report);
-		var date = new Date(report.report_date * 1000);
-		console.log(report.report_date);
-		var day = date.getDay();
-		var month = date.getMonth();
-		marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
-		marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}<br><a href='/report/${report.report_id}'>Visualizza segnalazione</a>`);
-		markerStartup.push(marker);
-		myMarkers = L.layerGroup(markerStartup).addTo(map);
-	})
+                console.log(report);
 
-})
+                marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
+                marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}<br>`);
+                markerStartup.push(marker);
+                markerDefaultGroup = L.layerGroup(markerStartup).addTo(map);
+            })
+
+        })
+
+    }
 
 }
+
+/* Class Functions Active */
+
+var little_marker = new Display();
+little_marker.staticDisplay();
+little_marker.dynamicDisplay();
 
 /* Filter functions */
 
-staticDisplay();
+class Filters {
 
-var regionMarkers = {
-	"Abruzzo": [], "Piemonte": [], "Sardegna": [], "Liguria": [], "Emilia-Romagna": [], "Valle D'Aosta": [],
-	"Trentino-Alto Adige": [],  "Lombardia": [], "Campania": [], "Lazio": [], "Veneto": [],  "Sicilia": [], "Calabria": [],
-	"Toscana": [], "Marche": [],  "Umbria": [], "Friuli Venezia Giulia": [], "Molise": [],  "Puglia": [], "Basilicata": []
+    regionFilter(regione) {
+        $.get("/api/data/getReports", reports => {
+
+            reports.map(report => {
+
+                map.removeLayer(markerDefaultGroup);
+
+                if (report.report_state == regione || regione == null) {
+                    marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
+                    marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}`);
+                    region_markers[regione].push(marker);
+
+                }
+
+            })
+        })
+    }
+
+    regionRemove(regione) {
+        $.get("/api/data/getReports", reports => {
+
+            reports.map(report => {
+                region_markers[regione].forEach(marker => map.removeLayer(marker));
+            })
+
+        })
+    }
+
+    titleFilter(title) {
+        var matcher = new RegExp("(\w*" + title + "\w*)", "i");
+
+        $.get("/api/data/getReports", reports => {
+
+            reports.map(report => {
+
+                map.removeLayer(markerDefaultGroup);
+
+                if (title == '') {
+                    map.removeLayer(markerDefaultGroup);
+                } else {
+                    if (matcher.test(report.report_title)) {
+                        marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
+                        marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}`);
+                        titleMarker.push(marker);
+                    }
+                }
+            })
+        })
+    }
+
+    titleRemove() {
+        $.get("/api/data/getReports", reports => {
+
+            reports.map(report => {
+                titleMarker.forEach(marker => map.removeLayer(marker));
+            })
+
+        })
+    }
+
+		addressFilter(address) {
+				var matcher = new RegExp("(\w*" + address + "\w*)", "i");
+
+        $.get("/api/data/getReports", reports => {
+
+            reports.map(report => {
+
+                map.removeLayer(markerDefaultGroup);
+
+                if (address == '') {
+                    map.removeLayer(markerDefaultGroup);
+                } else {
+                    if (matcher.test(report.report_address)) {
+                        marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
+                        marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}`);
+                        addressMarker.push(marker);
+                    }
+                }
+            })
+        })
+    }
+
+    addressRemove() {
+        $.get("/api/data/getReports", reports => {
+
+            reports.map(report => {
+                addressMarker.forEach(marker => map.removeLayer(marker));
+            })
+
+        })
+    }
+
+		dateFilter(date) {
+
+        $.get("/api/data/getReports", reports => {
+
+            reports.map(report => {
+
+								var marker_date = moment.unix(report.report_date.toString()).format('DD/MM/YYYY');
+                map.removeLayer(markerDefaultGroup);
+
+                if (date == '') {
+                    map.removeLayer(markerDefaultGroup);
+                } else {
+                    if (marker_date == date) {
+                        marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
+                        marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}`);
+                        addressMarker.push(marker);
+                    }
+                }
+            })
+        })
+    }
+
+		dateRemove() {
+        $.get("/api/data/getReports", reports => {
+
+            reports.map(report => {
+                addressMarker.forEach(marker => map.removeLayer(marker));
+            })
+
+        })
+    }
+
+		regionWiper() {
+    for (var value of Object.keys(region_markers)) {
+        region_markers[value].forEach(marker => map.removeLayer(marker));
+    }
 }
 
-function filtroRegione(regione){
-$.get("/api/data/getReports", reports => {
 
-	reports.map(report => {
+    restartMarkers() {
+        map.removeLayer(markerDefaultGroup);
+        markerDefaultGroup.addTo(map);
+    }
 
-		map.removeLayer(myMarkers);
-		if(report.report_state == regione || regione == null) {
-			var date = new Date(report.report_date * 1000);
-			console.log(report.report_date);
-			var day = date.getDay();
-			var month = date.getMonth();
-			marker = L.marker([report.report_latitude, report.report_longitude]).addTo(map);
-			marker.bindPopup(`<b>${report.report_title}</b><br>${moment.unix(report.report_date).format('DD/MM/YYYY')}`);
-			regionMarkers[regione].push(marker);
+		filterWiper() {										//rimuove tutti i filtri e gli input dei campi
+			this.regionWiper();
+			this.titleRemove();
+			this.addressRemove();
+			this.dateRemove();
+			$(".item").css({color: "#fff", background: "none"}).removeClass("selected");
+			$("#search-title")[0].value = '';
+			$("#search-address")[0].value = '';
+
 		}
 
-	})
-})
+		fullReset() {										//bottone azzera filtri
+			this.filterWiper();
+			this.restartMarkers();
+			$("#search-date")[0].value = '';
+		}
+
 }
 
-function removeRegione(regione){
-	$.get("/api/data/getReports", reports => {
-
-		reports.map(report => {
-				regionMarkers[regione].forEach(marker => map.removeLayer(marker));
-		})
-
-	})
-}
+var little_filter = new Filters();
 
 /* Filters Actions */
 
+/* Filtri per i bottoni delle regioni */
+
 $(".item").on('click', e => {
 
-	if(!$(e.currentTarget).hasClass("selected")){
-	 filtroRegione($(e.currentTarget).attr('name'));
- }else{
-	 removeRegione($(e.currentTarget).attr('name'));
-	 if($(".item.selected").length == 1){
-		 staticDisplay();
-	 }
- }
+    if (!$(e.currentTarget).hasClass("selected")) {
+        little_filter.regionFilter($(e.currentTarget).attr('name'));
+        little_filter.titleRemove();
+        little_filter.addressRemove();
+				little_filter.dateRemove();
+    } else {
+        little_filter.regionRemove($(e.currentTarget).attr('name'));
+        if ($(".item.selected").length == 1) {
+            little_filter.titleRemove();
+            little_filter.addressRemove();
+						little_filter.dateRemove();
+            little_filter.restartMarkers();
+        }
+    }
 
 })
 
-function restart() {
-	map.redraw();
+/* Filtri per i campi di testo */
+
+function titleSearch() {
+    var title_string = $("#search-title")[0].value;
+
+    little_filter.filterWiper();
+    little_filter.titleFilter(title_string);
+
 }
 
+function addressSearch() {
+    var address_string = $("#search-address")[0].value;
 
-/* -- Map selector -- */
+    little_filter.filterWiper();
+    little_filter.addressFilter(address_string);
 
-var layerLabels;
+}
 
- function setBasemap(basemap) {
-     if (layer) {
-       map.removeLayer(layer);
-     }
-     layer = L.esri.basemapLayer(basemap);
-     map.addLayer(layer);
-     if (layerLabels) {
-       map.removeLayer(layerLabels);
-     }
-     if (basemap === 'ShadedRelief') {
-       layerLabels = L.esri.basemapLayer(basemap + 'Labels');
-       map.addLayer(layerLabels);
-     } else if (basemap.includes('Imagery')) {
-       layerLabels = L.esri.basemapLayer('ImageryLabels');
-       map.addLayer(layerLabels);
-     }
-   }
+function dateSearch() {
+    var date_string = $("#search-date")[0].value;
 
-   function changeBasemap(basemaps){
-     var basemap = basemaps.value;
-     setBasemap(basemap);
-   }
-
-	 /* Geocoding Control */
-
-	var searchControl = L.esri.Geocoding.geosearch({
-		position: "topright",
-		placeholder: "Cerca una località"
-	}).addTo(map);
-  var results = L.layerGroup().addTo(map);
-
-  searchControl.on('results', function(data){
-    results.clearLayers();
-  });
+    little_filter.filterWiper();
+		little_filter.dateFilter(date_string);
+}
--- assets/js/misc.js
+++ assets/js/misc.js
@@ -4,68 +4,50 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
 
 /* -- Sidebar transition -- */
 
-  function togglebtn() {
-    $("#toggle-btn")[0].classList.toggle("change");
-  }
-  function openNav() {
-      $("#mySider")[0].classList.toggle("transition")
-      togglebtn();
-  }
+function togglebtn() {
+  $("#toggle-btn")[0].classList.toggle("change");
+}
+
+function openNav() {
+  $("#mySider")[0].classList.toggle("transition")
+  togglebtn();
+}
 
 /* -- Button animation -- */
 
   $(".item").click((e) => {
 
     if(!$(e.currentTarget).hasClass("selected"))
-      $(e.currentTarget).css({color: "white", background: "#202124"}).addClass("selected")
+      $(e.currentTarget).css({color: "black", background: "white"}).addClass("selected")
     else
-      $(e.currentTarget).css({color: "#202124", background: "none"}).removeClass("selected")
+      $(e.currentTarget).css({color: "white", background: "none"}).removeClass("selected")
 
   })
 
-/* -- Range slider -- */
-
-var rangeSlider = function(){
-  var slider = $('.box'),
-      range = $('.slider'),
-      value = $('.slider_value');
-
-  slider.each(function(){
-
-    value.each(function(){
-      var value = $(this).prev().attr('value');
-      $(this).html(value);
-    });
-
-    range.on('input', function(){
-      $(this).next(value).html(this.value);
-    });
-  });
-};
-
-rangeSlider();
 
 /* -- Date picker -- */
 
-$(function() {
+/* $(function() {
 
   $('input[name="datefilter"]').daterangepicker({
       opens: 'right',
       drops: 'up',
-      buttonClasses: "btn_picker",
+      format: "DD/MM/YYYY",
+      singleDatePicker: true,
+      showDropdowns: true,
       autoUpdateInput: false,
       autoApply: true,
       locale: {
@@ -74,11 +56,51 @@ $(function() {
   });
 
   $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
-      $(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
+      $(this).val(picker.startDate.format('DD/MM/YYYY'));
   });
 
   $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
       $(this).val('');
   });
 
+}); */
+
+
+/* -- Map selector -- */
+
+var layerLabels;
+
+function setBasemap(basemap) {
+    if (layer) {
+        map.removeLayer(layer);
+    }
+    layer = L.esri.basemapLayer(basemap);
+    map.addLayer(layer);
+    if (layerLabels) {
+        map.removeLayer(layerLabels);
+    }
+    if (basemap === 'ShadedRelief') {
+        layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+        map.addLayer(layerLabels);
+    } else if (basemap.includes('Imagery')) {
+        layerLabels = L.esri.basemapLayer('ImageryLabels');
+        map.addLayer(layerLabels);
+    }
+}
+
+function changeBasemap(basemaps) {
+    var basemap = basemaps.value;
+    setBasemap(basemap);
+}
+
+/* Geocoding Control */
+
+var searchControl = L.esri.Geocoding.geosearch({
+    position: "topright",
+    placeholder: "Cerca una località"
+}).addTo(map);
+var results = L.layerGroup().addTo(map);
+
+searchControl.on('results', function(data) {
+    results.clearLayers();
 });
